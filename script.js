4eac7e73206d99c7da43313cb5df185cc0d30b37
annadones <gatonix.sirah@gmail.com>
2025-04-14 08:41:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//Criar o vetor ARRAY
+
+var nomes = ["Ana", "Bruno", "Carla", "Diego", "Eduarda", "Felipe", "Giovana", "Henrique", "Isabela", "João", "Karla",
+ "Lucas", "Marina", "Nicolas", "Olívia", "Paulo", "Quésia", "Rafael", "Sofia", "Tiago", "Úrsula", "Vinícius", "Wesley",
+  "Xuxa", "Yasmin", "Zeca", "Amanda", "Caio", "Lívia", "Mateus"]
+
+var nomeEscolhido = parseInt(prompt("Digite um número de 0 a 30"))
+
+//concatenar
+document.write ("Nome escolhido"  + nomes[nomeEscolhido])
